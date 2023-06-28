65442d2d473bfcb65ee558ef000c1ae4f7eded9d
elian-dev <geordymemdoza@gmail.com>
2023-06-27 23:30:26 -0600

    feat: :sparkles: Components added

---
+++ src/components/data.js
@@ -0,0 +1,176 @@
+const data = [
+    {
+        id: 1,
+        name: 'Arcade',
+        category: 'monthly',
+        price: 9,
+        image: './assets/icon-arcade.svg',
+        desc: ``,
+        free: '',
+        addons: [
+            {
+                id: 1,
+                name: 'Online service',
+                desc: 'Access to multiplayer games',
+                time: '1 month'
+            },
+            {
+                id: 2,
+                name: 'Larger storage',
+                desc: 'Extra 1TB of cloud save',
+                time: '2 month'
+            },
+            {
+                id: 3,
+                name: 'Customizable profile',
+                desc: 'Custom theme on your profile',
+                time: '2 month'
+            },
+        ]       
+    },
+    {
+        id: 2,
+        name: 'Advanced',
+        category: 'monthly',
+        price: 12,
+        image: './images/advanced.jpg',
+        desc: ``,
+        free: '',
+        addons: [
+            {
+                id: 1,
+                name: 'Online service',
+                desc: 'Access to multiplayer games',
+                time: '1 month'
+            },
+            {
+                id: 2,
+                name: 'Larger storage',
+                desc: 'Extra 1TB of cloud save',
+                time: '2 month'
+            },
+            {
+                id: 3,
+                name: 'Customizable profile',
+                desc: 'Custom theme on your profile',
+                time: '2 month'
+            },
+        ] 
+    },
+    {
+        id: 3,
+        name: 'Pro',
+        category: 'monthly',
+        price: 15,
+        image: './images/pro.jpg',
+        desc: ``,
+        free: '',
+        addons: [
+            {
+                id: 1,
+                name: 'Online service',
+                desc: 'Access to multiplayer games',
+                time: '1 month'
+            },
+            {
+                id: 2,
+                name: 'Larger storage',
+                desc: 'Extra 1TB of cloud save',
+                time: '2 month'
+            },
+            {
+                id: 3,
+                name: 'Customizable profile',
+                desc: 'Custom theme on your profile',
+                time: '2 month'
+            },
+        ] 
+    },
+    {
+        id: 4,
+        name: 'Arcade',
+        category: 'yearly',
+        price: 99,
+        image: './images/arcade.jpg',
+        desc: ``,
+        free: '2 months free',
+        addons: [
+            {
+                id: 1,
+                name: 'Online service',
+                desc: 'Access to multiplayer games',
+                time: '10 year'
+            },
+            {
+                id: 2,
+                name: 'Larger storage',
+                desc: 'Extra 1TB of cloud save',
+                time: '20 year'
+            },
+            {
+                id: 3,
+                name: 'Customizable profile',
+                desc: 'Custom theme on your profile',
+                time: '20 year'
+            },
+        ] 
+    },
+    {
+        id: 5,
+        name: 'Advanced',
+        category: 'yearly',
+        price: 120,
+        image: './images/advanced.jpg',
+        desc: ``,
+        free: '2 months free',
+        addons: [
+            {
+                id: 1,
+                name: 'Online service',
+                desc: 'Access to multiplayer games',
+                time: '10 year'
+            },
+            {
+                id: 2,
+                name: 'Larger storage',
+                desc: 'Extra 1TB of cloud save',
+                time: '20 year'
+            },
+            {
+                id: 3,
+                name: 'Customizable profile',
+                desc: 'Custom theme on your profile',
+                time: '20 year'
+            },
+        ]
+    },  
+    {
+        id: 6,
+        name: 'Pro',
+        category: 'yearly',
+        price: 150,
+        image: './images/pro.jpg',
+        desc: ``,
+        free: '2 months free',
+        addons: [
+            {
+                id: 1,
+                name: 'Online service',
+                desc: 'Access to multiplayer games',
+                time: '10 year'
+            },
+            {
+                id: 2,
+                name: 'Larger storage',
+                desc: 'Extra 1TB of cloud save',
+                time: '20 year'
+            },
+            {
+                id: 3,
+                name: 'Customizable profile',
+                desc: 'Custom theme on your profile',
+                time: '20 year'
+            },
+        ]
+    },
+]
