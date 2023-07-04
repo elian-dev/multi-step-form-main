88554be0e800b0a7477d1849a96d4daa8c1bde72
elian_dev <geordymemdoza@gmail.com>
2023-07-03 21:13:28 -0600

    fix: :bug: Fix some layout issues

--- src/scripts/utils/FormValidator.js
+++ src/scripts/utils/FormValidator.js
@@ -12,7 +12,6 @@ const state = getData();
 
 export function FormValidator(formData, data) {
     const areEmptiesErrors = IsEmpty(data);
-    const areEmailInputs = document.querySelectorAll('input[type="email"]');
 
     // Remove all errors
     const errors = document.querySelectorAll('.error');
@@ -22,8 +21,6 @@ export function FormValidator(formData, data) {
     }
 
     if (areEmptiesErrors.length > 0 ) {
-        console.log('There are empty inputs');
-
         // Identify the input group that has the error
         const emptyInputs = areEmptiesErrors.map((error) => {
             return document.querySelector(`[name="${error}"]`).parentElement;
@@ -35,20 +32,19 @@ export function FormValidator(formData, data) {
         });
 
     }  else {
-        console.log('There are no empty inputs');
-            // Append the error message if the email is invalid
-            const input = document.querySelector('input[type="email"]');
-            const inputParent = input.parentElement;
+        // Append the error message if the email is invalid
+        const input = document.querySelector('input[type="email"]');
+        const inputParent = input.parentElement;
 
-            if (!IsValidEmail(input.value)) {
-                AppendError(inputParent, 'Please enter a valid email address');
-            } else {
-                // ✨ Set the form data
-                setFormData(state, data);
+        if (!IsValidEmail(input.value)) {
+            AppendError(inputParent, 'Please enter a valid email address');
+        } else {
+            // ✨ Set the form data
+            setFormData(state, data);
 
-                // ✨ Go to the next step
-                nextStep(1);
-            }
+            // ✨ Go to the next step
+            nextStep(1);
+        }
 
     }
 }
