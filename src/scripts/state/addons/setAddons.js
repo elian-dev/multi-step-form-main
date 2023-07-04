742ed29646c83a8c9e859e49b09c33e0da35fb0c
elian_dev <geordymemdoza@gmail.com>
2023-07-03 23:35:13 -0600

    fix: :bug: Fix when selected addons and go back keep selected

--- src/scripts/state/addons/setAddons.js
+++ src/scripts/state/addons/setAddons.js
@@ -1,4 +1,6 @@
 export function setAddons(state, data) {
+    // Save the keys in array
+    data = Object.keys(data);
     state.addonsSelected = data;
 
     sessionStorage.setItem('data', JSON.stringify(state));
--- src/scripts/state/setData.js
+++ src/scripts/state/setData.js
@@ -18,7 +18,7 @@ export function setData() {
             },
 
             // Addons	
-            addonsSelected: []
+            addonsSelected: null
         }
     };
 
