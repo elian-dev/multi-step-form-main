e86201c7b516d698a5a196a3bab11460a17eb0a6
elian_dev <geordymemdoza@gmail.com>
2023-07-03 23:55:49 -0600

    fix:

--- src/scripts/state/addons/setAddons.js
+++ src/scripts/state/addons/setAddons.js
@@ -1,6 +1,9 @@
+import { NULL } from "sass";
+
 export function setAddons(state, data) {
     // Save the keys in array
-    data = Object.keys(data);
+    data !== null ? data = Object.keys(data) : data = null;
+    
     state.addonsSelected = data;
 
     sessionStorage.setItem('data', JSON.stringify(state));
